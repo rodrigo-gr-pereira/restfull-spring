35d902ec8deb0e6a97324b48e75f09bfb607f586
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2024-10-28 22:08:35 -0300

    Inicial class ok

--- src/main/java/restfull/dio/model/Account.java
+++ src/main/java/restfull/dio/model/Account.java
@@ -1,4 +1,43 @@
 package restfull.dio.model;
 
+import java.math.BigDecimal;
+
 public class Account {
+
+    private Long id;
+    private String nome;
+    private String agency;
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
 }
--- src/main/java/restfull/dio/model/User.java
+++ src/main/java/restfull/dio/model/User.java
@@ -1,4 +1,52 @@
 package restfull.dio.model;
 
+import java.util.List;
+
 public class User {
+
+    private Long id;
+    private String nome;
+    private Account account;
+    private List<Feature> features;
+    private List<News> news;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
 }
